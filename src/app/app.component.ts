ca65026244f91cab330637034ee7b71ea58f4c1e
Richard Robles <rrobles9112@gmail.com>
2017-04-27 01:13:13 -0500

    creado el header y footer inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
-@Component({
+import {Component, OnInit} from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./../assets/css/foundation.css','app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+
+  public ngOnInit() {
+
+
+  }
+
 }
+
